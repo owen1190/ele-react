a202c04a81fc978a7ba3f6eea0e830eeb49d70b8
yu <owen1190@sina.cn>
2017-07-01 21:18:01 +0800

    add footer

---
+++ app/components/Footer/index.js
@@ -0,0 +1,15 @@
+import React,{Component} from 'react'
+
+class Footer extends Component{
+  render(){
+    renturn(
+      <div className="footer">
+        <section className="">外卖</section>
+        <section className="">搜索</section>
+        <section className="">订单</section>
+        <section className="">我的</section>
+      </div>   
+    )
+  }
+}
+export default Footer;
