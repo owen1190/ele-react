2a0c8d5d02a6285159e9140a1a3744ee49e7bbd8
owen-yu <owen1190@sina.cn>
2017-07-04 21:38:30 +0800

    add link

--- app/components/Footer/index.js
+++ app/components/Footer/index.js
@@ -1,13 +1,21 @@
 import React,{Component} from 'react'
 import './style.less'
+import {Link} from 'react-router-dom'
 class Footer extends Component{
+  constructor(props){
+    super(props);
+    this.goToAddress=this.goToAddress.bind(this);
+  }
+  goToAddress(path) {
+        this.props.history.push(path);
+  }
   render(){
     return(
-      <div className="footer">
-        <section className="mistie">外卖</section>
-        <section className="search">搜索</section>
-        <section className="order">订单</section>
-        <section className="profile">我的</section>
+      <div className="footer">        
+          <Link to="/"><p className="order" >外卖</p></Link> 
+          <Link to="/search/:geoHash"><p className="discovery" >发现</p></Link> 
+          <p className="order" >订单</p>        
+          <Link to="/profile"><p className="profile" >我的</p></Link> 
       </div>   
     )
   }
